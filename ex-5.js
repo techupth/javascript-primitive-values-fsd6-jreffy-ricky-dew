ac459aca9035e38c32a9f8b47d4f2bd3f6c3e8dd
Wascharapong Phummon <rickyharvey.w@gmail.com>
2024-05-09 15:15:13 +0700

    feat: Primitive-Values

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,11 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let originalPrice = 27000;
+let discountRate = 0.25;
+let discountAmount = originalPrice * discountRate;
+let finalPrice = originalPrice - discountAmount;
+console.log(originalPrice);
+console.log(discountAmount);
+console.log(finalPrice);
+
