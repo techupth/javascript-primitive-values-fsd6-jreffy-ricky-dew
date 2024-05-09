0b6f0f69504f812c7ff92ec96f798f78ca13bc67
Surakit <surakitkunkayun@gmail.com>
2024-05-09 14:33:21 +0700

    feat: finished assignment

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = "27000";
+let discount = totalPrice * (25/100);
+let totalPriceAfterDiscount = totalPrice-discount;
+console.log(totalPriceAfterDiscount);
